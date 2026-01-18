544e4a140960ffccef7e87699410a68cb05493a2
jrines <joshuahrines@gmail.com>
2026-01-18 14:53:28 -0800

    minor: runtime 2hr

--- engine/inference/run_inference_lakes.sh
+++ engine/inference/run_inference_lakes.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=inference_lakes
 #SBATCH --output=/oak/stanford/groups/cyaolai/JoshRines/sherlock/sherlock_cloudytile/logs/%x_%A_%a.out
 #SBATCH --error=/oak/stanford/groups/cyaolai/JoshRines/sherlock/sherlock_cloudytile/logs/%x_%A_%a.err
-#SBATCH --time=08:00:00
+#SBATCH --time=02:00:00
 #SBATCH -p serc
 #SBATCH --gpus=1
 #SBATCH --nodes=1
